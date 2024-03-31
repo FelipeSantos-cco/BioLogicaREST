1a08895aa107d0711c2d16a3e16e2beb72756abe
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-03-31 13:31:25 -0300

    :tada: Add Project Spring Boot

---
+++ bio-logica-rest/src/test/java/biologicarest/BioLogicaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package biologicarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioLogicaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
